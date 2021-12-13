c8582ced0768018bee990fbe89a94c94b8ca2818
Jon Coe <jonmcoe@gmail.com>
2021-12-12 21:00:03 -0500

    convert p06 to use map. solve p06b

--- src/main/scala/problems/Problem06.scala
+++ src/main/scala/problems/Problem06.scala
@@ -4,19 +4,29 @@ import common.BaseProblem
 
 object Problem06 extends BaseProblem(6) {
 
-  val initial = readInput().next().split(',').map(_.toInt).toSeq
+  val initial =
+    readInput()
+      .next()
+      .split(',')
+      .map(_.toInt)
+      .groupBy(identity)
+      .view
+      .mapValues(_.length.toLong)
+      .toMap
 
-  def step(l: Seq[Int]): Seq[Int] = {
-    val numNew = l.count(_ == 0)
-    val existing = l.map(x => if (x == 0) 6 else x - 1)
-    existing ++ List.fill(numNew)(8)
+  def step(m: Map[Int, Long]): Map[Int, Long] = {
+    val numNew = m.getOrElse(0, 0L)
+    val existing = m.map {
+      case (t, freq) => if (t == 0) (6, freq) else (t - 1, freq)
+    } // TODO: possible to handle the merging case successfully?
+    existing + (8 -> numNew) + (6 -> (numNew + m.getOrElse(7, 0L)))
   }
 
   def applyRepeatedly[A](l: A, f: A => A, n: Int): A = {
     (1 to n).foldLeft(l) { case (prev, _) => f(prev) }
   }
 
-  def solutionA = applyRepeatedly(initial, step, 80).length
+  def solutionA = applyRepeatedly(initial, step, 80).values.sum
 
-  def solutionB = applyRepeatedly(initial, step, 256).length // OOM
+  def solutionB = applyRepeatedly(initial, step, 256).values.sum
 }
